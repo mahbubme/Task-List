7c46f741a607543fc8dd6d766a78235c90df7426
mahbubme <mail@mahbub.me>
2016-08-15 22:52:46 +0600

    added required files for frontend design

--- src/index.php
+++ src/index.php
@@ -1,5 +1,35 @@
-<?php 
+<!DOCTYPE html>
 
-	echo "Hello";
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
 
-?>
+		<link rel="stylesheet" href="assets/stylesheets/app.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+
+
+		<!-- jQuery -->
+		<script src="assets/js/jquery-3.1.0.min.js"></script>
+		<!-- Bootstrap JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Material Design JavaScript -->
+    <script src="assets/js/material.js"></script>
+		<script src="assets/js/ripples.js"></script>
+
+		<!-- Custom Script -->
+		<script src="assets/js/app.js"></script>
+    <script>$.material.init();</script>
+	</body>
+</html>
